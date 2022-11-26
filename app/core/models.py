d15576b9d2971b3081d097475bf90d859c9953d7
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2022-11-26 19:27:57 +0100

    ttd of authusermodel

---
+++ app/core/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser, PermissionsMixin, BaseUserManager)
+
+
+class AuthUserManager(BaseUserManager):
+    '''Auth manager for the AuthUserModel'''
+
+    def create_user(self, email, password=None, **extra_field):
+        '''create and return a new user'''
+        if not email:
+            raise ValueError("Email field is required!")
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_field)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+
+class AuthUserModel(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(max_length=100, unique=True)
+    name = models.CharField(max_length=255, null=False)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = AuthUserManager()
+    USERNAME_FIELD = 'email'
---
+++ app/core/tests/test_models.py
@@ -0,0 +1,29 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+
+class TestModels(TestCase):
+    '''test cases for models'''
+
+    def test_create_user_with_email_successful(self):
+        '''test successfully creation of users with email and password'''
+        email = 'test@domain.com'
+        password = 'TestPass123'
+        user = get_user_model().objects.create_user(
+            email=email, password=password
+        )
+
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
+
+    def test_create_user_email_is_normalized(self):
+        '''test that the second part of user email is normalized'''
+        email = 'test@DOMAIN.COM'
+        user = get_user_model().objects.create_user(email, 'test123')
+
+        self.assertEqual(user.email, email.lower())
+
+    def test_new_user_invalid_email(self):
+        '''test that error is thrown if no email is provided'''
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, 'test123')
